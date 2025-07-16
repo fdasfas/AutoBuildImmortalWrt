046517ae048d80879edefe423d7e1097942cdd3e
fdasfas <109186807+fdasfas@users.noreply.github.com>
2025-07-16 12:19:18 +0800

    Update build.sh
    取消dock等不必要插件

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -52,15 +52,15 @@ PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
-PACKAGES="$PACKAGES luci-app-openclash"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+# PACKAGES="$PACKAGES luci-app-openclash"
+# PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 # docker
-PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
-PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 
 # ============= imm仓库外的第三方插件===============
 # ===== 若启用 请打开PACKAGES前面的注释==============
